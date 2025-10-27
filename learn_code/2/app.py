b565214f93372102e839ca5bf7253b6fe032eeeb
onlyheng <xuyixiang1314@163.com>
2025-10-27 15:01:33 +0800

    Fix outdated code

--- learn_code/2/app.py
+++ learn_code/2/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "welcome to My WatchList"
+    return '<center><h1>welcome to My WatchList</h1><img src="http://helloflask.com/totoro.gif"></center>'
+
+
+@app.route('/user/<name>')
+def user_usage(name):
+    return f'User: {escape(name)}'
 
 
 if __name__ == '__main__':
